29b3896ce66538818ab145b162185a1000b1e696
MrWalshyType2 <mwalsh@academytrainee.com>
2020-12-15 16:52:41 +0000

    initial project commit

---
+++ src/main/java/com/brewery/microserviceappsb/MicroserviceAppSbApplication.java
@@ -0,0 +1,13 @@
+package com.brewery.microserviceappsb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceAppSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceAppSbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brewery/microserviceappsb/MicroserviceAppSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brewery.microserviceappsb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceAppSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
